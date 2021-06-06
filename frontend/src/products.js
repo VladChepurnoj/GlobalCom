bd5eee399c0e2b50a1a20b73f11e35059839f868
Vlad <chepurnojv@gmail.com>
2021-06-06 13:26:01 +0300

    crated HomeScreen, Product and Rating components

---
+++ frontend/src/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: "1",
+    name: "Huawei P Smart 2021 NFC 128GB Green",
+    image: "/images/huaweiPSmart.png",
+    description:
+      'Экран (6.67", IPS, 2400x1080) / HiSilicon Kirin 710A (2.0 ГГц + 1.7 ГГц) / две основные камеры: 48 Мп + 8 Мп + 2 Мп + 2 Мп, фронтальная камера: 8 Мп / RAM 4 ГБ / 128 ГБ встроенной памяти + microSD / 3G / LTE / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 10 / 5000 мА*ч',
+    brand: "Huawei",
+    category: "Мобільний телефон / смартфон",
+    price: 4999.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Apple iPhone 12 Pro Max 128GB",
+    image: "/images/iphone12Pro.jpeg",
+    description:
+      'Экран (6.7", OLED (Super Retina XDR), 2778x1284) / Apple A14 Bionic / тройная основная камера: 12 Мп + 12 Мп + 12 Мп, фронтальная камера: 12 Мп / 128 ГБ встроенной памяти / 3G / LTE / 5G / GPS / Nano-SIM / iOS 14',
+    brand: "Apple",
+    category: "Мобільний телефон / смартфон",
+    price: 39499.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Motorola G9 Power 4/128",
+    image: "/images/motorola.jpg",
+    description:
+      'Экран (6.8", IPS, 1640x720) / Qualcomm Snapdragon 662 (2.0 ГГц) / тройная основная камера: 64 Мп + 2 Мп + 2 Мп, фронтальная камера: 16 Мп / RAM 4 ГБ / 128 ГБ встроенной памяти + microSD (до 512 ГБ) / 3G / LTE / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 10 / 6000 мА*ч',
+    brand: "Motorola",
+    category: "Мобільний телефон / смартфон",
+    price: 4999.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Poco M3 Pro 6/128GB",
+    image: "/images/pocoM3.jpg",
+    description:
+      'Экран (6.5", 2400x1080) / MediaTek Dimensity 700 (2.2 ГГц) / тройная основная камера: 48 Мп + 2 Мп + 2 Мп, фронтальная 8 Мп / RAM 6 ГБ / 128 ГБ встроенной памяти / 3G / LTE / 5G / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 11 / 5000 мА*ч',
+    brand: "Poco",
+    category: "Мобільний телефон / смартфон",
+    price: 6999.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Realme 7 Pro 8/128GB",
+    image: "/images/realme7Pro.jpg",
+    description:
+      'Экран (6.4", Super AMOLED, 2400x1080) / Qualcomm Snapdragon 720G (2.3 ГГц) / основная квадро-камера: 64 Мп + 8 Мп + Макро + Портрет, фронтальная камера: 32 Мп / RAM 8 ГБ / 128 ГБ встроенной памяти + microSD (до 256 ГБ) / 3G / LTE / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 10 / 4500 мА*ч',
+    brand: "Realme",
+    category: "Мобільний телефон / смартфон",
+    price: 8999.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Samsung Galaxy M12 4/64GB",
+    image: "/images/samsungGalaxyM12.jpg",
+    description:
+      'Экран (6.5", PLS, 1600x720) / Samsung Exynos 850 (2.0 ГГц) / основная квадро-камера: 48 Мп + 5 Мп + 2 Мп + 2 Мп, фронтальная камера: 8 Мп / RAM 4 ГБ / 64 ГБ встроенной памяти + microSD (до 1 ТБ) / 3G / LTE / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 11 / 5000 мА*ч',
+    brand: "Samsung",
+    category: "Мобільний телефон / смартфон",
+    price: 4599.99,
+    countInStock: 15,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "7",
+    name: "Xiaomi Mi 10T 6/128GB ",
+    image: "/images/xiaomiMi10T.jpg",
+    description:
+      'Экран (6.67", 2400x1080) / Qualcomm Snapdragon 865 (2.84 ГГц) / тройная основная камера: 64 Мп + 13 Мп + 5 Мп, фронтальная 20 Мп / RAM 6 ГБ / 128 ГБ встроенной памяти / 3G / LTE / 5G / GPS / поддержка 2х SIM-карт (Nano-SIM) / Android 10 / 5000 мА*ч',
+    brand: "Xiaomi",
+    category: "Мобільний телефон / смартфон",
+    price: 12999.99,
+    countInStock: 10,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
